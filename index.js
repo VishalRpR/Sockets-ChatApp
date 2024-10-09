f69b5dbb99aa7dd8f10889be6c2dfae4211e9478
vishal <vishrrajput02@gmail.com>
2024-10-09 17:26:02 +0530

    Intialized the server side rendering along with the group and chat model for ppersistent DB for chat and various rooms/groups

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express=require('express');
 const app=express();
  const connect=require('./config/db-config')
+ const Group = require('./models/group');
+const Chat = require('./models/chat');
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
 
 const http=require('http')
 const server = http.createServer(app);
@@ -9,6 +13,39 @@ const {Server} = require('socket.io');
 const io = new Server(server);
 
 app.use('/',express.static(__dirname+'/public'));
+app.set('view engine', 'ejs');
+
+app.get('/chat/:roomid/:user',async(req,res)=>{
+
+  const group=await Group.findById(req.params.roomid);
+  const chats=await Chat.find({
+    roomid:req.params.roomid
+  });
+  console.log(chats);
+  
+ res.render('index',{
+  roomid:req.params.roomid,
+  user:req.params.user,
+  groupname: group.name,
+  previousmsgs:chats
+
+ })
+
+
+})
+
+
+app.get('/group',async(req,res)=>{
+      res.render('group')
+})
+
+app.post('/group',async(req,res)=>{
+  console.log(req.body);
+  await Group.create({
+    name:req.body.name
+  });
+  res.redirect('/group')
+})
 
 io.on('connection', (socket) => {
     console.log(`a user connected ${socket.id}`);
@@ -16,12 +53,22 @@ io.on('connection', (socket) => {
         console.log(`user disconnected ${socket.id}`);
       });
 
+    socket.on('join room',(data)=>{
+         console.log('joining the room',data.roomid);
+         socket.join(data.roomid);
+    })
 
     
-    socket.on('chat message', (msg) => {
-          console.log('message: ' + msg);
+    socket.on('chat message', async(data) => {
+          console.log('message: ' + data);
 
-          io.emit('chat message',socket.id+'--'+msg)
+          const chat=await Chat.create({
+            roomid:data.roomid,
+            sender:data.sender,
+            content:data.message
+          })
+
+          io.to(data.roomid).emit('chat message',data)
         })
 
    
